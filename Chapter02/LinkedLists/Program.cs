bde59156ca827a34612c309dc695172a649e0de8
Leandro Arruda <lvarruda@gmail.com>
2026-09-07 18:39:46 -0400

    add: Linked lists

---
+++ Chapter02/LinkedLists/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace LinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Page pageFirst = new Page() { Content = "Nowadays, there are various (...)" };
+            Page pageSecond = new Page() { Content = "Application development is (...)" };
+            Page pageThird = new Page() { Content = "A lot of applications (...)" };
+            Page pageFourth = new Page() { Content = "Do you know that modern (...)" };
+            Page pageFifth = new Page() { Content = "While developing applications (...)" };
+            Page pageSixth = new Page() { Content = "Could you imagine your (...)" };
+
+            LinkedList<Page> pages = new LinkedList<Page>();
+            pages.AddLast(pageSecond);
+            LinkedListNode<Page> nodePageForth = pages.AddLast(pageFourth);
+            pages.AddLast(pageSixth);
+            pages.AddFirst(pageFirst);
+            pages.AddBefore(nodePageForth, pageThird);
+            pages.AddAfter(nodePageForth, pageFifth);
+
+            LinkedListNode<Page> current = pages.First;
+            int number = 1;
+            while (current != null)
+            {
+                Console.Clear();
+                string numberString = $"- {number} -";
+                int leadingSpaces = (90 - numberString.Length) / 2;
+                Console.WriteLine(numberString.PadLeft(leadingSpaces + numberString.Length));
+                Console.WriteLine();
+
+                string content = current.Value.Content;
+                for (int i = 0; i < content.Length; i += 90)
+                {
+                    string line = content.Substring(i);
+                    line = line.Length > 90 ? line.Substring(0, 90) : line;
+                    Console.WriteLine(line);
+                }
+
+                Console.WriteLine();
+
+                Console.WriteLine($"Quote from page Packt Book {Environment.NewLine}{current.Value.Content}");
+
+                Console.WriteLine();
+
+                Console.Write(current.Previous != null ? "Press 'P' to go to the previous page. " : GetSpaces(14));
+
+                Console.Write(current.Next != null ? "Press 'N' to go to the next page. " : string.Empty);
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.P:
+                        if (current.Previous != null)
+                        {
+                            current = current.Previous;
+                            number--;
+                        }
+                        break;
+                    case ConsoleKey.N:
+                        if (current.Next != null)
+                        {
+                            current = current.Next;
+                            number++;
+                        }
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+
+        private static string GetSpaces(int number)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < number; i++)
+            {
+                result += " ";
+            }
+            return result;
+        }
+    }
+}
--- Chapter02/SortedLists/Program.cs
+++ Chapter02/SortedLists/Program.cs
@@ -3,6 +3,18 @@
     class Program
     {
         static void Main(string[] args)
-        { }
+        {
+            SortedList<string, Person> people = new SortedList<string, Person>();
+
+            people.Add("Marcia", new Person() { Name = "Marcia", Country = CountryEnum.ES, Age = 29 });
+            people.Add("Sabine", new Person() { Name = "Sabine", Country = CountryEnum.IT, Age = 25 });
+            people.Add("Anne", new Person() { Name = "Anne", Country = CountryEnum.CA, Age = 31 });
+
+            foreach (KeyValuePair<string, Person> person in people)
+            {
+                Console.WriteLine($"{person.Value.Name} {person.Value.Age} years old from {person.Value.Country}.");
+            }
+
+        }
     }
 }
